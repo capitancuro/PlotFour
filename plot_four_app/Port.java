1d0bbd1aff512b6f56fdaaa2cbc3ae6c9502283f
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-06-01 00:44:55 -0700

    Update Port.java

--- plot_four_app/Port.java
+++ plot_four_app/Port.java
@@ -1,7 +1,6 @@
 package plot_four_app;
 
 import javafx.scene.Scene; //Needed to work with content accessed via the Port
-import javafx.scene.paint.Color;
 
 
 import plot_four_app.StartMenu;
@@ -9,7 +8,7 @@ import plot_four_app.StartMenu;
 public class Port extends Scene {
 	
 	public Port() {
-		super(new StartMenu(), 1000, 1000, Color.rgb(0,0, 0));
+		super(new StartMenu(), 1000, 1000);
 	}
 
 }
