9c9af720d5b00b0af6a0d16b99aa0161e6680086
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-06-01 01:25:53 -0700

    Update Port.java

--- plot_four_app/Port.java
+++ plot_four_app/Port.java
@@ -1,11 +1,22 @@
 package plot_four_app;
 
 import javafx.scene.Scene; //Needed to work with content accessed via the Port
-
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 
 import plot_four_app.StartMenu;
 
 public class Port extends Scene {
+	
+
+	public class Controller {
+		
+		private final Scene scene = null;
+		private final Parent[] contexts = null;
+		
+		public Controller(Scene scene) {
+		}
+	}
 	
 	public Port() {
 		super(new StartMenu(), 1000, 1000);
