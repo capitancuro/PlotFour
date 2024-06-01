3a42fc6db0df6b94ff60493c844053db5209c37b
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-05-31 21:47:51 -0700

    Update and rename Home_Scene.java to View.java

---
+++ plot_four_app/View.java
@@ -0,0 +1,13 @@
+package plot_four_app;
+
+import javafx.scene.Scene; //Class needed to work with content accessed via the window.
+import javafx.scene.paint.Color;
+
+public class View extends Scene {
+	
+	public View() {
+		super(new Group(), 1000, 1000, Color.rgb(0,0, 0));
+		// TODO Auto-generated constructor stub
+	}
+
+}
