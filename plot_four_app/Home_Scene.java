e5ead563ffa1b69ec075fb32fa6c104d6d50d179
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-05-31 21:14:33 -0700

    Create Home_Scene.java

---
+++ plot_four_app/Home_Scene.java
@@ -0,0 +1,16 @@
+package plot_four_app;
+
+import javafx.scene.Scene;															//Class needed to work with content accessed via the window.
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+public class Home_Scene extends Scene {
+
+	public Home_Scene(Parent arg0, double arg1, double arg2, Paint arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
