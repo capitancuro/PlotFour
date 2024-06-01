6d1d70d457e29ffe308c5506919b4a3b30c80b53
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-06-01 01:02:19 -0700

    Update ContextController.java

--- plot_four_app/ContextController.java
+++ plot_four_app/ContextController.java
@@ -1,9 +1,12 @@
 package plot_four_app;
 
-import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
 
 public class ContextController {
+	private final Scene scene = null;
+	private final Parent[] contexts = null;
 	
-	
-
+	public ContextController(Scene scene) {
+	}
 }
