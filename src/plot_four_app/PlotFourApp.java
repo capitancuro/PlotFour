73aa898d35b6412c20b712cd4917ae451322b014
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-06-05 00:01:56 -0700

    Update PlotFourApp.java

--- src/plot_four_app/PlotFourApp.java
+++ src/plot_four_app/PlotFourApp.java
@@ -1,8 +1,9 @@
 package plot_four_app;
 
 //JAVA FX DEPENDENCIES
-import javafx.application.Application; //Class needed for the Application.
-import javafx.stage.Stage; //Class needed for window(s) of the application.
+import javafx.application.Application; //Class needed for the Application
+import javafx.stage.Stage; //Class needed for window(s) of the application
+import javafx.scene.image.Image;
 
 //PLOT FOUR CLASSES
 import plot_four_app.*;
@@ -15,7 +16,8 @@ public class PlotFourApp extends Application {
 	}
 	
 	@Override
-	public void start(Stage window){
+	public void start(Stage window) {
+		window.getIcons().add(new Image("/assets/Plot_Four.png"));
 		window.setTitle("Plot Four");
 		window.setScene(port);
 		window.show();
