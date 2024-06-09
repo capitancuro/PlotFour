79411c8cf24ceb46718c23bad5559c6e50c7322d
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-06-09 15:32:03 -0700

    Update Port.java

--- src/plot_four_app/Port.java
+++ src/plot_four_app/Port.java
@@ -3,20 +3,25 @@ package plot_four_app;
 import assets.AssetsManager;
 
 import javafx.scene.Scene; //Needed to work with content accessed via the Port
+import contexts.Controller;
+import contexts.PlotFour;
 import contexts.StartMenu;
 import javafx.scene.Parent;
 import javafx.scene.paint.Color;
 
-import plot_four_app.PlotFour;
-import plot_four_app.Controller;
-
 public class Port extends Scene {
+	
+	private double width = 0;
+	private double height = 0;
 	
 	public AssetsManager assetsManager = null;
 	public Controller controller = null;
 	
-	public Port(AssetsManager assetsManager, Controller controller) {
-		super(new StartMenu(assetsManager, controller), 1000, 1000, Color.BLACK);
+	public Port(double width, double height, AssetsManager assetsManager, Controller controller) {
+		super(new StartMenu(assetsManager, controller), width, height, Color.BLACK);
+		
+		this.width = width;
+		this.height = height;
 		
 		this.assetsManager = assetsManager;
 		this.controller = controller;
@@ -27,6 +32,6 @@ public class Port extends Scene {
 	
 	private void setContexts() {
 		controller.contexts[0] = this.getRoot();
-		controller.contexts[1] = new PlotFour();
+		controller.contexts[1] = new PlotFour(width, height);
 	}
 }
