f051c159f7feeebafa76b3845d416c06e51e1cbe
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-06-16 14:29:58 -0700

    Update Controller.java

--- src/contexts/Controller.java
+++ src/contexts/Controller.java
@@ -1,13 +1,15 @@
 package contexts;
 
+import java.util.HashMap;
+
 import javafx.scene.Parent;
 import plot_four_app.Port;
 public class Controller {
 	
 	public Port port = null;
-	public Parent[] contexts = null;
+	public HashMap<String, Parent> contexts = null;
 	
 	public void changeContext() {
-		port.setRoot(contexts[1]);
+		port.setRoot(contexts.get("Plot Four"));
 	}
 }
