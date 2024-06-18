f82352f9c2cce3100a967603c3e0fa2795f4b5a2
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-06-18 16:04:59 -0700

    Update AssetsManager.java

--- src/assets/AssetsManager.java
+++ src/assets/AssetsManager.java
@@ -1,16 +1,15 @@
 package assets;
 
 import java.io.InputStream;
+import java.io.File;  
 
 public class AssetsManager {
 	
-	public InputStream font = null;
-	
-	public AssetsManager() {
-		font = AssetsManager.class.getResourceAsStream("PressStart2P-Regular.ttf");
-	}
-	
 	public InputStream getFont() {
 		return AssetsManager.class.getResourceAsStream("PressStart2P-Regular.ttf");
+	}
+	
+	public String getDropAudio() {
+		return new File("audio").listFiles()[0].toURI().toString();
 	}
 }
