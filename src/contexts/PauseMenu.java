f2eed757bc4a430e1d100b1dfd1cd6d4e4e4f445
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-06-26 02:47:32 -0700

    Update PauseMenu.java

--- src/contexts/PauseMenu.java
+++ src/contexts/PauseMenu.java
@@ -1,8 +1,21 @@
 package contexts;
 
-import javafx.scene.Group;
+import assets.AssetsManager;
+
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.control.Button;
 
-public class PauseMenu extends Group {
+public class PauseMenu extends VBox {
+	
+	private final Button home = null;
+	private final Button back = null;
+	
+	public void PauseMenu() {
+		
+	}
 
 }
