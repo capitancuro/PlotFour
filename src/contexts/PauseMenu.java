5abf26588cf971b1c3ad8c2a4277ba8021e38331
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-06-26 05:24:48 -0700

    Update PauseMenu.java

--- src/contexts/PauseMenu.java
+++ src/contexts/PauseMenu.java
@@ -5,9 +5,7 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 
@@ -16,10 +14,10 @@ public class PauseMenu extends VBox {
 	AssetsManager assetsManager = null;
 	Controller controller = null;
 	
-	private final Button home = null;
-	private final Button back = null;
+	private Button home = null;
+	private Button back = null;
 	
-	public void PauseMenu(AssetsManager assetsManager, Controller controller) {
+	public PauseMenu(AssetsManager assetsManager, Controller controller) {
 		
 		this.assetsManager = assetsManager;
 		this.controller = controller;
@@ -27,6 +25,47 @@ public class PauseMenu extends VBox {
 		setAlignment(Pos.CENTER);
 		setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
 		setSpacing(25);
+		
+		home = new Button("HOME");
+		home.setFont(Font.loadFont(assetsManager.getFont(), 15));
+		home.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
+		home.setTextFill(Color.WHITE);
+		
+		home.setOnMouseClicked(event -> {
+			controller.changeContext("Start Menu");
+		});
+		
+		home.setOnMouseEntered(event -> {
+			home.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+			home.setTextFill(Color.BLACK);
+		});
+		
+		home.setOnMouseExited(event -> {
+			home.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
+			home.setTextFill(Color.WHITE);
+		});
+		
+		back = new Button("BACK");
+		back.setFont(Font.loadFont(assetsManager.getFont(), 15));
+		back.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
+		back.setTextFill(Color.WHITE);
+		
+		back.setOnMouseClicked(event -> {
+			controller.changeContext("Plot Four");
+		});
+		
+		back.setOnMouseEntered(event -> {
+			back.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+			back.setTextFill(Color.BLACK);
+		});
+		
+		back.setOnMouseExited(event -> {
+			back.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
+			back.setTextFill(Color.WHITE);
+		});
+		
+		getChildren().add(home);
+		getChildren().add(back);
 	}
 
 }
