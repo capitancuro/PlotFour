e2691cd4173b5082ec24192b2ccd08c6a11ea1cd
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-06-30 03:37:07 -0700

    Update Port.java

--- src/plot_four_app/Port.java
+++ src/plot_four_app/Port.java
@@ -5,14 +5,15 @@ import java.util.HashMap;
 import assets.AssetsManager;
 
 import javafx.scene.Scene; //Needed to work with content accessed via the Port
-import javafx.scene.Parent;
-import javafx.scene.paint.Color;
+import javafx.scene.Parent; //Needed to store subclasses of parent
+import javafx.scene.paint.Color; //Needed to set color of the root node
 
-import contexts.Controller;
+import contexts.Controller; //Needed to control interactions between different contexts
 import contexts.PlotFour;
 import contexts.StartMenu;
 import contexts.PauseMenu;
 
+//The scene used to store different contexts of the Plot Four app
 public class Port extends Scene {
 	
 	private double width = 0;
@@ -21,21 +22,21 @@ public class Port extends Scene {
 	public AssetsManager assetsManager = null;
 	public Controller controller = null;
 
-	
 	public Port(double width, double height, AssetsManager assetsManager, Controller controller) {
 		super(new StartMenu(assetsManager, controller), width, height, Color.BLACK);
 		
 		this.width = width;
 		this.height = height;
 		
-		this.assetsManager = assetsManager;
+		this.assetsManager = assetsManager; 
 		this.controller = controller;
 		
 		controller.contexts = new HashMap<String, Parent>();
 		setContexts();
 	}
 	
-	private void setContexts() {
+	//Creates an instance of every context used in the app
+	public void setContexts() {
 		controller.contexts.put("Start Menu", getRoot());
 		controller.contexts.put("Pause Menu", new PauseMenu(assetsManager, controller));
 		controller.contexts.put("Plot Four", new PlotFour(width, height, assetsManager, controller));
